4fa0e2876ec0931028aaef90d9568a985a727fd8
aman22o8 <amankumar.kumar369@gmail.com>
2024-07-03 11:04:49 +0530

    all check

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express=require('express')
 const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
@@ -14,32 +15,36 @@ mongoose.connect(DATABASE,{useNewUrlParser:true,useUnifiedTopology:true}).then((
     console.log(e)
 })
 
-app.use(cors());
+app.use(cors({origin:true}));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on localhost://${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
 const todoSchema=new mongoose.Schema({
     title:{type:String,required:true},
     description:String,
-    dateCreated:String
+    dateCreated:String,
+    id:{type:String}
 })
 
 const todo=new mongoose.model("todoList",todoSchema)
 
+//ADDING NEW TODOS
+
 app.post("/api/v1/new",async(req,res)=>{
-    const {title,desc}=req.body
+   
+    const {title,description,id}=req.body
 
       try {
         const dateCreated= new Date().toLocaleString()
-        // const newtodoAdded=new todo({title:title,description:desc,dateCreated:dateCreated})
+        // const newtodoAdded=new todo({title:title,description:description,dateCreated:dateCreated})
         // const todoResult= await newtodoAdded.save() 
-        const todoResult = await  todo.create({title:title,description:desc,dateCreated:dateCreated})
+        const todoResult = await  todo.create({title:title,description:description,dateCreated:dateCreated,id:id})
       
         res.status(200).json({
-          success:true,todoResult,
+          success:true,addedtodo:todoResult,
           message:"Successfully Added"
         })        
       } catch (error) {
@@ -47,7 +52,7 @@ app.post("/api/v1/new",async(req,res)=>{
       }
 
 })
-
+// GETTING ALL TODOS
 app.get("/api/v1/",async (req,res)=>{
 
     try {
@@ -63,6 +68,8 @@ app.get("/api/v1/",async (req,res)=>{
        
 })
 
+//UPDATING TODOS
+
 app.put("/api/v1/updateproduct/:id",async (req,res)=>{
     try {
         let todoupdateresult=await todo.findById(req.params.id)
@@ -72,8 +79,10 @@ app.put("/api/v1/updateproduct/:id",async (req,res)=>{
                 message:"Product not found"
             })
         }
+        const dateUpdated= new Date().toLocaleString()
+        const {title,description,id}=req.body
 
-        todoupdateresult=await todo.findByIdAndUpdate(req.params.id,req.body,{new:true,useFindAndModify:true,runValidators:true})
+        todoupdateresult=await todo.findByIdAndUpdate(req.params.id,{title:title,description:description,dateCreated:dateUpdated,id:id},{new:true,useFindAndModify:true,runValidators:true})
     
         res.status(200).json({
             message:"Successfully Updated",
@@ -87,11 +96,13 @@ app.put("/api/v1/updateproduct/:id",async (req,res)=>{
     }
     
 })
+//DELETE ALL TODOS
 
-app.delete('/api/v1/deleteproduct/:id',async(req,res)=>{
+app.delete('/api/v1/deleteproduct/:uniid',async(req,res)=>{
     console.log(req.params)
+    const {uniid}=req.params
     try {
-        let todoupdateresult=await todo.findById(req.params.id)
+        let todoupdateresult=await todo.findById(uniid)
 
         if(!todoupdateresult){
             res.status(400).json({
@@ -100,7 +111,7 @@ app.delete('/api/v1/deleteproduct/:id',async(req,res)=>{
             })
         }
 
-        await todoupdateresult.deleteOne({ _id: req.params.id})
+        await todoupdateresult.deleteOne({_id: uniid})
     
         res.status(200).json({
             message:"Successfully deleted"    
